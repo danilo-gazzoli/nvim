5238827c9d2c1edab6990e755dc938bd1347b15d
danilo-gazzoli <dmartinsgazzoli@gmail.com>
2025-01-03 00:15:01 -0300

    add plugins: alpha, bufferline, comment, indent, lualine, misc, none-ls and snacks

--- init.lua
+++ init.lua
@@ -20,5 +20,10 @@ require('lazy').setup({
     require 'plugins.gitsigns',
     require 'plugins.bufferline',
     require 'plugins.lualine',
-    require 'plugins.lsp',
+    require 'plugins.snacks',
+    require 'plugins.alpha',
+    require 'plugins.comment',
+    require 'plugins.indent-blankline',
+    require 'plugins.misc',
+    require 'plugins.none-ls',
 })
