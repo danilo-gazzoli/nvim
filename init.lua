c04710b6b143767ddbb712e17de1ffbda37c56e4
danilo-gazzoli <dmartinsgazzoli@gmail.com>
2025-01-06 22:16:21 -0300

    chore: init.lua and lazy-lock alterations

--- init.lua
+++ init.lua
@@ -4,28 +4,30 @@ require("core.keymaps")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    error("Error cloning lazy.nvim:\n" .. out)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  require("plugins.neotree"),
-  require("plugins.colortheme"),
-  require("plugins.treesitter"),
-  require("plugins.telescope"),
-  require("plugins.gitsigns"),
-  require("plugins.bufferline"),
-  require("plugins.lualine"),
-  require("plugins.snacks"),
-  require("plugins.alpha"),
-  require("plugins.comment"),
-  require("plugins.indent-blankline"),
-  require("plugins.misc"),
-  require 'plugins.none-ls',
-  require("plugins.lsp"),
-  require("plugins.nvim-cmp"),
+
+	{ import = "lazyvim.plugins.extras.lsp.none-ls" },
+	require("plugins.neotree"),
+	require("plugins.colortheme"),
+	require("plugins.treesitter"),
+	require("plugins.telescope"),
+	require("plugins.gitsigns"),
+	require("plugins.bufferline"),
+	require("plugins.lualine"),
+	require("plugins.snacks"),
+	require("plugins.alpha"),
+	require("plugins.comment"),
+	require("plugins.indent-blankline"),
+	require("plugins.misc"),
+	require("plugins.none-ls"),
+	require("plugins.lsp"),
+	require("plugins.nvim-cmp"),
 })
