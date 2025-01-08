4e303c19be1bbec01eaab7a6562beeb0b17aad64
danilo-gazzoli <dmartinsgazzoli@gmail.com>
2025-01-08 14:57:32 -0300

    feat: tab autocompletation and ctrl + backspace implementation

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,33 +1,64 @@
 return {
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
     dependencies = {
-        'hrsh7th/cmp-nvim-lsp',    -- Integração com LSP
-        'hrsh7th/cmp-buffer',      -- Completador para buffer atual
-        'hrsh7th/cmp-path',        -- Completador para caminhos
-        'hrsh7th/cmp-cmdline',     -- Completador para linha de comando
-        'L3MON4D3/LuaSnip',        -- Snippets engine
-        'saadparwaiz1/cmp_luasnip' -- Integração com LuaSnip
+        "hrsh7th/cmp-nvim-lsp", -- Integração com LSP
+        "hrsh7th/cmp-buffer", -- Completador para buffer atual
+        "hrsh7th/cmp-path",   -- Completador para caminhos
+        "hrsh7th/cmp-cmdline", -- Completador para linha de comando
+        "L3MON4D3/LuaSnip",   -- Snippets engine
+        "saadparwaiz1/cmp_luasnip", -- Integração com LuaSnip
     },
     config = function()
-        local cmp = require('cmp')
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+
+        -- Função para verificar se há palavras antes do cursor
+        local has_words_before = function()
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
+
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require('luasnip').lsp_expand(args.body)
+                    luasnip.lsp_expand(args.body)
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Aceita automaticamente a sugestão selecionada
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Aceita automaticamente a sugestão selecionada
+
+                -- Suporte para Tab e Shift+Tab
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    elseif has_words_before() then
+                        cmp.complete()
+                    else
+                        fallback() -- Comportamento padrão
+                    end
+                end, { "i", "s" }),
+
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback() -- Comportamento padrão
+                    end
+                end, { "i", "s" }),
             }),
             sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' },
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
             }, {
-                { name = 'buffer' },
+                { name = "buffer" },
             }),
         })
     end,
