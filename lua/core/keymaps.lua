dd4cf918630122afa6c8041d4ade7ce12c56bc04
danilo-gazzoli <dmartinsgazzoli@gmail.com>
2025-01-27 22:43:43 -0300

    feat: nvim dap plugin keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -102,3 +102,13 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", opts) -- Scroll up and center
 -- Search and center results
 vim.keymap.set("n", "n", "nzzzv", opts) -- Go to next match and center
 vim.keymap.set("n", "N", "Nzzzv", opts) -- Go to previous match and center
+
+-- nvim dap
+vim.keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>", opts)
+vim.keymap.set("n", "<F10>", ":lua require'dap'.step_over()<CR>", opts)
+vim.keymap.set("n", "<F11>", ":lua require'dap'.step_into()<CR>", opts)
+vim.keymap.set("n", "<F12>", ":lua require'dap'.step_out()<CR>", opts)
+vim.keymap.set("n", "<Leader>b", ":lua require'dap'.toggle_breakpoint()<CR>", opts)
+vim.keymap.set("n", "<Leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", opts)
+vim.keymap.set("n", "<Leader>dr", ":lua require'dap'.repl.open()<CR>", opts)
+vim.keymap.set("n", "<Leader>dl", ":lua require'dap'.run_last()<CR>", opts)
